fd7000dc76acfff7d3f602e775ab81fc48d4b256
Michael Onoja <onoja16@gmail.com>
2023-07-27 18:16:02 +0100

    Updated file added

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,7 +6,9 @@ UTF-8 Validation
 
 def validUTF8(data):
     """
-    method that determines if a given data set represents a valid UTF-8 encoding    
+    data: a list of integers
+    Return: True if data is a valid UTF-8
+    encoding, else return False
     """
     byte_count = 0
 
